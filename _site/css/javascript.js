475d758ec1f38881b73e332c34ff36a9e76a7612
ettore90 <besartshyti@gmail.com>
2014-06-24 12:19:30 +0000

    Stuff

---
+++ _site/css/javascript.js
@@ -0,0 +1,26 @@
+  $.ajax({
+    url: "http://content.guardianapis.com/search?section=news",
+    dataType: "jsonp",
+    success: function(data) {
+      $.each(data.response.results, function () {
+        $( "#news" ).append("<li>" + this['webTitle'] + "</li>");
+      });
+    }});
+
+  $.ajax({
+    url: "http://content.guardianapis.com/search?section=football",
+    dataType: "jsonp",
+    success: function(data) {
+      $.each(data.response.results, function () {
+        $( "#football" ).append("<li>" + this['webTitle'] + "</li>");
+      });
+    }});
+
+  $.ajax({
+    url: "http://content.guardianapis.com/search?section=travel",
+    dataType: "jsonp",
+    success: function(data) {
+      $.each(data.response.results, function () {
+        $( "#travel" ).append("<li>" + this['webTitle'] + "</li>");
+      });
+    }});

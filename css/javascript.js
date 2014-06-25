74132e3167c8ab5d98bfb59d6744097edee6c676
Besart Shyti <besartshyti@gmail.com>
2014-06-25 17:06:28 +0100

    Made the javascript code more automatic

--- css/javascript.js
+++ css/javascript.js
@@ -2,32 +2,17 @@
     $( "#tabs" ).tabs();
   });
 
-  $.ajax({
-    url: "http://content.guardianapis.com/search?section=news&show-fields=trailText",
-    dataType: "jsonp",
-    success: function(data) {
-      $.each(data.response.results, function () {
-        $( "#news" ).append('<a href="' + this['webUrl'] + '"><h5>' + this['webTitle'] + '<h5></a>');
-        $( "#news" ).append("<ul>" + this['fields'].trailText + "</ul>");
-      });
-    }});
-
-  $.ajax({
-    url: "http://content.guardianapis.com/search?section=football&show-fields=trailText",
-    dataType: "jsonp",
-    success: function(data) {
-      $.each(data.response.results, function () {
-        $( "#football" ).append('<a href="' + this['webUrl'] + '"><h5>' + this['webTitle'] + '<h5></a>');
-        $( "#football" ).append("<ul>" + this['fields'].trailText + "</ul>");
-      });
-    }});
-
-  $.ajax({
-    url: "http://content.guardianapis.com/search?section=travel&show-fields=trailText",
-    dataType: "jsonp",
-    success: function(data) {
-      $.each(data.response.results, function () {
-        $( "#travel" ).append('<a href="' + this['webUrl'] + '"><h5>' + this['webTitle'] + '<h5></a>');
-        $( "#travel" ).append("<ul>" + this['fields'].trailText + "</ul>");
-      });
-    }});
+  $(document).ready(function() {
+    var list={"1":"uk-news","2":"football","3":"travel"};
+    $.each(list, function(key, value) {
+      $.ajax({
+        url: "http://content.guardianapis.com/search?section=" + value + "&show-fields=trailText",
+        dataType: "jsonp",
+        success: function(data) {
+          $.each(data.response.results, function () {
+            $( "#" + value ).append('<a href="' + this['webUrl'] + '"><h5>' + this['webTitle'] + '<h5></a>');
+            $( "#" + value ).append("<ul>" + this['fields'].trailText + "</ul>");
+          });
+        }});
+    });
+  });

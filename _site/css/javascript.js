9e624216952d6bc834aa623d31d07dfe26279499
ettore90 <besartshyti@gmail.com>
2014-07-09 15:52:04 +0000

    stuff

--- _site/css/javascript.js
+++ _site/css/javascript.js
@@ -14,6 +14,17 @@
           });
         }});
     });
+    
+    $.ajax({
+        url: "http://obscure-ridge-8042.herokuapp.com/",
+        dataType: "json",
+
+        success: function(data) {
+          $.each(data.stuff, function () {
+            $( "#tweets" ).append('<div class="img"><a target="_blank" href="klematis_big.htm"><img src="' + this['img'] + '" alt="Klematis" width="300" height="250"></a><div class="desc">' + this['name'] +  '</div></div>');
+          });
+       }});
+    
   });
 
 

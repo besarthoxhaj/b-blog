ce531c3240d35a7d6689f991875633b95d207f97
Besart Shyti <besartshyti@gmail.com>
2014-07-10 16:58:44 +0100

    Server json

--- css/javascript.js
+++ css/javascript.js
@@ -20,7 +20,7 @@
         dataType: "json",
 
         success: function(data) {
-          $.each(data.stuff, function () {
+          $.each(data.statuses, function () {
             $( "#tweets" ).append('<div class="stuff1"><a target="_blank" href="klematis_big.htm"><img src="' + this['img'] + '" alt="Klematis" width="300" height="250"></a><div class="desc">' + this['name'] +  '</div></div>');
           });
        }});
